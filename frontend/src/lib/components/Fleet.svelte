7138100df3f12c51d922394d490bf8e8048a8794
apollinelbcr <apolline.leb@gmail.com>
2024-02-22 18:10:34 -0500

    added available and booked vehicule count

---
+++ frontend/src/lib/components/Fleet.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+
+    export let available: string;
+    export let booked: string;
+    
+    export let href: string;
+</script>
+
+<div class=" w-1/4 absolute top-42 right-28 h-52 space-y-4">
+
+<a href={href} class=" w-full h-20 rounded-3xl bg-white justify-left flex flex-row ">
+    <div class="text-xl p-2"></div>
+    <div class="pl-4 pt-2">
+        <h2 class="text-wrap text-2xl " > Available Vehicules: </h2>
+    </div>
+
+    <div class="text-xl p-2"></div>
+    <div class="pl-2 pt-4 pb-2">
+        <h2 class="text-wrap  text-5xl text-orange-700 " > {available} </h2>
+    </div>
+
+</a>    
+
+<a href={href} class=" w-full h-20 rounded-3xl bg-white justify-left flex flex-row ">
+    
+    <div class="text-xl p-2"></div>
+    <div class="pl-4 pt-2">
+        <h2 class="text-wrap text-2xl " > Booked Vehicules: </h2>
+    </div>
+
+    <div class="text-xl p-2"></div>
+    <div class="pl-6 pt-4 pb-2 ">
+        <h2 class="text-wrap text-5xl text-orange-700 " > {booked} </h2>
+    </div>
+
+</a> 
+</div> 
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -3,6 +3,7 @@
     import RecentUserWraper from "../lib/components/RecentUserWraper.svelte";
     import RecentUser from "../lib/components/RecentUser.svelte";
     import Title from "../lib/components/Title.svelte";
+    import Fleet from "../lib/components/Fleet.svelte";
 </script>
 
 <main class="md:px-20">
@@ -11,6 +12,11 @@
         <RecentUser name="Amanda Smith" text="Mar 1 – Mar 8" bookingref="#0003" cartype="Sedan" href="/"/>
         <RecentUser name="Alex Wilson" text="Feb 22 – Feb 28" bookingref="#0002" cartype="Luxury" href="/"/>
         <RecentUser name="Anna Woods" text="Apr 15 – Apr 23" bookingref="#0001" cartype="SUV" href="/"/>
+    
     </RecentUserWraper>
+    
+    <Fleet available="18" booked="10" href="/" />
+    
+
 </Title>
 </main>
