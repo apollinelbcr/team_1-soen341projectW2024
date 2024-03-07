eedf82e2d04a93767cf68e84effc4bb975b5fa97
Zineb Bamouh <zineb.bamouh@gmail.com>
2024-03-07 13:06:36 -0500

    adding

---
+++ frontend/src/lib/components/carBox.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+
+    export let price: number;
+    export let type: string;
+    export let category: string;
+    export let src: string;
+    export let href: string;
+</script>
---
+++ frontend/src/routes/Information/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import {type Car} from "$lib/Model/Car";
+    import carBox from "$lib/components/carBox.svelte"
+
+</script>
+<main>
+
+
+</main>
+<style>
+
+</style>
