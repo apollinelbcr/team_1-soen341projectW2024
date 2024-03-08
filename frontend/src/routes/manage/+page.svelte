93c7856c7f5a3cd649ea886df1506e7bfcce1934
loveydovey <asmaaimade0@gmail.com>
2024-03-08 12:37:57 -0500

    Manage reservation page

---
+++ frontend/src/routes/manage/+page.svelte
@@ -0,0 +1,33 @@
+<div class="fixed w-100% pt-4 px-16">
+    <a href="">Logo here</a>
+</div>
+<div class="flex px-12 py-16">
+    
+    <div class=" fixed p-4 w-60 bg-gray-300 mr-2">
+        <div class="pt-5 pb-5">
+            <a href="">Dashboard</a>
+        </div>
+        <hr>
+        <div class="pt-5 pb-5">
+            <a href="/manage">My Account</a>
+        </div>
+        <hr>
+        <div class="pt-5 pb-5">
+            <a href="">Reviews</a>
+        </div>
+        <hr>
+        <div class="pt-5 pb-5">
+            <a href="/">My Reservations</a>
+        </div>
+        <hr>
+        <div class="pt-5 pb-5">
+            <a href="">Settings</a>
+        </div>
+    </div>
+    <div class="flex flex-col w-auto ml-64 flex-1">
+        
+        <h2>test for route href</h2>
+
+    </div>
+    
+</div>
