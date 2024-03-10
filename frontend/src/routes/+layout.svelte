ec9f27fcdb20017b690af00fd6948aaa83a685dd
Zineb Bamouh <zineb.bamouh@gmail.com>
2024-03-10 13:05:59 -0400

    Update +layout.svelte

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -2,7 +2,7 @@
   import "../app.css";
   import Header from "$lib/components/layout/Header.svelte";
   import Footer from "$lib/components/layout/Footer.svelte";
-  import {Toaster} from "svelte-sonner";
+  importgit  {Toaster} from "svelte-sonner";
 </script>
 
 <Toaster closeButton/>
