a9f205f86261ced92cda88cacbb095bad1d2ebff
beaudelaire <Beaudelaire.dev@gmail.com>
2024-03-09 21:07:47 -0500

    implementing filtering

--- frontend/src/lib/components/MainSearchBar.svelte
+++ frontend/src/lib/components/MainSearchBar.svelte
@@ -4,7 +4,7 @@
     const today: string = new Date().toISOString();
     let pickupLocation = '';
     let dropOffLocation = 'Same as pick-up';
-    let pickupDate = today.slice(0, 10); // default to today's date
+    let pickupDate = today.slice(0, 10);
     let dropOffDate = today.slice(0, 10);
     let pickupTime = "10:30";
     let dropOffTime = "10:30";
@@ -12,7 +12,6 @@
 </script>
 
 <div class="mt-5 sm:mt-10 sm:mx-10 sm:border border-gray-400 border-solid rounded-3xl p-5">
-
     <form class="flex flex-col" action="/search">
         <div class="grid grid-cols-6 gap-4 lg:grid-cols-4">
             <input name="pickup_location"
@@ -22,7 +21,7 @@
             <input name="dropoff_location"
                    type="text" placeholder="Drop-off"
                    class="focus:outline-none focus:border-blue-500 border rounded p-2 col-span-6 md:col-span-3 lg:col-span-2 h-12 border-gray-500"
-                   value={dropOffLocation}/>
+                   bind:value={dropOffLocation}/>
 
             <div class="border rounded p-2 col-span-3  lg:col-span-1 h-14 border-gray-500 relative">
                 <div class="absolute top-0">
@@ -52,6 +51,3 @@
         <button type="submit" class="mt-4 bg-blue-700 text-white p-4 rounded-full w-fit max-sm:w-full">Search</button>
     </form>
 </div>
-
-<style>
-</style>
