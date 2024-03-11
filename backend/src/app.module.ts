4f67da365aceb2e6f8a3aeb662c1a39a4ce1fe99
loveydovey <asmaaimade0@gmail.com>
2024-03-11 19:56:40 -0400

    backend

---
+++ backend/src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { VehiclesModule } from './vehicles/vehicles.module';
+import { ReservationsModule } from './reservations/reservations.modules';
+
+@Module({
+  imports: [
+    VehiclesModule,  ReservationsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'rentify',
+      autoLoadEntities: true, //entities translate to tables ... 
+      synchronize: true, // keep db schema in sync
+    }),
+
+  ]
+})
+export class AppModule {}
