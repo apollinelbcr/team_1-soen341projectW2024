04b6835b61c3bbba1757c1bcbe6ad965788489de
apollinelbcr <apolline.leb@gmail.com>
2024-03-11 13:56:31 -0400

    user database create

---
+++ backend/dist/vehicles/vehicle-categories.enum.d.ts
@@ -0,0 +1,11 @@
+export declare enum VehicleCategories {
+    COMPACT = "COMPACT",
+    STANDARD = "STANDARD",
+    INTERMEDIATE = "INTERMEDIATE",
+    ELECTRIC = "ELECTRIC",
+    HYBRID = "HYBRID",
+    CLASS_1 = "CLASS 1",
+    CLASS_2 = "CLASS 2",
+    MINI = "MINI",
+    LARGE = "LARGE"
+}
