76c50713eabcf27689ed1b255e4c06be6f79e070
Zineb Bamouh <zineb.bamouh@gmail.com>
2024-03-10 21:28:30 -0400

    nestjs

---
+++ src/reservations/reservations.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { ReservationsController } from './reservations.controller';
+import { ReservationsController } from './reservations.controller';
+import { ReservationsController } from './reservations.controller';
+import { ReservationsController } from './reservations.controller';
+import { ReservationsController } from './reservations.controller';
+
+@Module({
+  controllers: [ReservationsController]
+})
+export class ReservationsModule {}
