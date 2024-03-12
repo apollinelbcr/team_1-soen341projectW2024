c07688267a32394f0db16ccbf6dccfccc94085af
beaudelaire <Beaudelaire.dev@gmail.com>
2024-03-11 21:53:41 -0400

    resolve conflict

--- frontend/src/routes/admin/manage-reservations/create-reservation/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import CreateVehicle from "../../../../components/reservations/CreateReservations.svelte";
-
-</script>
-<svelte:head>
-	<title>Add a Vehicle</title>
-</svelte:head>
-
-<CreateVehicle title="Add a vehicle" />
