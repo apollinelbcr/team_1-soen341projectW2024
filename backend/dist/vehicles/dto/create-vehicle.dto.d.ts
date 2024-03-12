13a4b70761af751d6077ca669cd65a20ef4a7427
Beaudelaire Tsoungui Nzodoumkouo <63948003+Tsounguinzo@users.noreply.github.com>
2024-03-11 20:54:17 -0400

    Delete backend/dist directory

--- backend/dist/vehicles/dto/create-vehicle.dto.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class CreateVehicleDto {
-    name_vehicle: string;
-    image: string;
-    vehicle_type: string;
-    vehicle_category: string;
-    vehicle_transmission: string;
-}
--- backend/dist/vehicles/dto/update-vehicle-status.dto.d.ts
+++
@@ -1,4 +0,0 @@
-import { VehicleStatus } from "../vehicle-status.enum";
-export declare class UpdateVehicleStatusDto {
-    status: VehicleStatus;
-}
