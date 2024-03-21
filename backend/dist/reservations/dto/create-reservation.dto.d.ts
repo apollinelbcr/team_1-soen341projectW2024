4c231dd4346c102d2e45e5024b2431ad9122ee67
loveydovey <asmaaimade0@gmail.com>
2024-03-20 22:50:11 -0400

    .

---
+++ backend/dist/reservations/dto/create-reservation.dto.d.ts
@@ -0,0 +1,13 @@
+import { Extras } from '../reservations-extras.enum';
+export declare class CreateReservationDto {
+    email: string;
+    vehicle_name: string;
+    pickup_date: string;
+    pickup_time: string;
+    dropoff_date: string;
+    dropoff_time: string;
+    pickup_location: string;
+    dropoff_location: string;
+    price: number;
+    extras: Extras;
+}
