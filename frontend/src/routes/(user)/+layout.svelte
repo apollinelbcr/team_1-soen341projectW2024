f4b4df7cadf3311a3602f95d1360a7b55c0d8807
beaudelaire <Beaudelaire.dev@gmail.com>
2024-03-29 09:07:54 -0400

    user account page update

--- frontend/src/routes/(user)/+layout.svelte
+++ frontend/src/routes/(user)/+layout.svelte
@@ -1,5 +1,18 @@
 <script>
-    import "../../app.css";
+  import "../../app.css";
+  import Header from "$lib/components/layout/Header.svelte";
+  import Footer from "$lib/components/layout/Footer.svelte";
+  import {Toaster} from "svelte-sonner";
+  import {page} from "$app/stores";
 </script>
 
-  <slot />
+<Toaster closeButton/>
+<Header/>
+
+<main class="md:px-10 min-h-[calc(100vh-19vh)]">
+<slot />
+</main>
+
+{#if !$page.url.pathname.match(/^(\/signup|\/login)$/)}
+<Footer/>
+{/if}
